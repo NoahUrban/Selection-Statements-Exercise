6d3f5b9dea7e98475b251f47b76b1b6d963e57cf
“Noah <urbannoah@yahoo.com>
2024-06-05 11:23:27 -0400

    selection statment exercise updated

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,30 +4,39 @@
     {
         static void Main(string[] args)
         {
-            int favNum = 31;
+            var rnd = new Random();
+            int returnValue = rnd.Next(1, 11);
+            bool Guess = false;
 
             Console.WriteLine("Guess what my favorite number is!\n" +
-                "Hint: it is between 0 and 100");
+                "Hint: it is between 0 and 10");
 
             Console.WriteLine();
 
-            int userInput = int.Parse(Console.ReadLine());
+            int userInput = 0;
 
             Console.WriteLine();
 
-            if (userInput > favNum)
+            while (!Guess)
             {
-                Console.WriteLine("Too High");
-            }
-            else if (userInput < favNum)
-            {
-                Console.WriteLine("Too Low");
-            }
-            else
-            {
-                Console.WriteLine("Correct!");
-            }
+                userInput = int.Parse(Console.ReadLine());
 
+                if (userInput > returnValue)
+                {
+                    Console.WriteLine("Too High");
+                    Console.WriteLine();
+                }
+                else if (userInput < returnValue)
+                {
+                    Console.WriteLine("Too Low");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Correct!");
+                    Guess = true;
+                }
+            }
         }
     }
 }
